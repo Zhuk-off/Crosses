523a5b15e03b6bc4622313dc03619f799d5263e1
Alexander <zhukoff.alexander@gmail.com>
2023-06-17 13:45:31 +0300

    refactor: Add useReducer

--- components/game-new/game.jsx
+++ components/game-new/game.jsx
@@ -5,23 +5,33 @@ import { GameLayout } from "./ui/game-layout";
 import { GameTitle } from "./ui/game-title";
 import { PlayerInfo } from "./ui/player-info";
 import { GameMoveInfo } from "./ui/game-move-info";
-import { useGameState } from "./model/use-game-state";
 import { GameCell } from "./ui/game-cell";
 import GameOverModal from "./ui/game-over-modal";
+import { useReducer } from "react";
+import { GAME_STATE_ACTIONS, gameStateReduser, initGameState } from "./model/game-state-reducer";
+import { computeWinnerSymbol } from "./model/compute-winner-symbol";
+import { computeWinner } from "./model/compute-winner";
+import { getNextMove } from "./model/get-next-move";
 
 const PLAYERS_COUNT = 2;
 
 export function Game() {
-  const {
-    cells,
-    currentMove,
-    nextMove,
-    handleCellClick,
+  const [gameState, dispatch] = useReducer(
+    gameStateReduser,
+    { playersCount: PLAYERS_COUNT },
+    initGameState
+  );
+
+  const winnerSequence = computeWinner(gameState);
+  const nextMove = getNextMove(gameState, []);
+  const winnerSymbol = computeWinnerSymbol(gameState, {
     winnerSequence,
-    winnerSymbol,
-  } = useGameState(PLAYERS_COUNT);
+    nextMove,
+  });
 
   const winnerPlayer = PLAYERS.find((player) => player.symbol === winnerSymbol);
+
+  const { cells, currentMove } = gameState;
 
   return (
     <>
@@ -47,7 +57,9 @@ export function Game() {
         gameCells={cells.map((cell, index) => (
           <GameCell
             key={index}
-            onClick={() => handleCellClick(index)}
+            onClick={() =>
+              dispatch({ type: GAME_STATE_ACTIONS.CELL_CLICK, index })
+            }
             isWinner={winnerSequence?.includes(index)}
             disabled={!!winnerSymbol}
             symbol={cell}
@@ -55,7 +67,7 @@ export function Game() {
         ))}
       />
       <GameOverModal
-      winnerName={winnerPlayer?.name}
+        winnerName={winnerPlayer?.name}
         players={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
           <PlayerInfo
             key={player.id}
